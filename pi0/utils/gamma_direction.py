71276e4f259bb3271ec2a531d3440827a463b4fc
Peter S Madigan <pmadigan@hep-gpu01.slac.stanford.edu>
2019-06-20 14:46:29 -0700

    Implement first pass of gamma selection algorithms

--- pi0/utils/gamma_direction.py
+++ pi0/utils/gamma_direction.py
@@ -1,20 +1,104 @@
 '''
 This module contains methods associated with inferring the direction of a gamma shower
 
-'''
+''' 
+import numpy as np
+from sklearn.cluster import DBSCAN
 
-def do_calculation(data, start, length=5.):
+def norm(xyz_hit, xyz_vtx):
+    '''
+    Calculates the euclidean distance from xyz_vtx to each hit in xyz_hit
+    Args:
+        xyz_hit - an Nx3 array of hit locations (x,y,z)
+        xyz_vtx - a length 3 (x,y,z) location of the point to calculate distances from
+    Return:
+        an Nx1 array of distances for each hit location to the vtx
+    '''
+    xyz_d = xyz_hit[:,:3] - xyz_vtx
+    return np.array([np.sqrt(xyz[0]**2 + xyz[1]**2 + xyz[2]**2) for xyz in xyz_d])
+
+def dbscan_find_primary(xyz_hit, eps, min_samples):
+    '''
+    Performs DBSCAN on the specified hits
+    Args:
+        xyz_hit - Nx3 array of (x,y,z) of each hit
+        eps - epsilon for dbscan
+        min_samples - min_samples for dbscan
+    Return:
+        Nx1 mask for most common cluster, if not noise, else returns Nx1 array of True
+    
+    '''
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(xyz_hit)
+    core_hits_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_hits_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+#     print('dbscan labels: {}'.format(labels))
+    (unique_labels, counts) = np.unique(labels[labels != -1], return_counts=True)
+    max_label = -1
+    if any(counts >= 0):
+        max_label = unique_labels[np.argmax(counts)]
+    if not max_label == -1:
+        return labels == max_label & core_hits_mask
+    return labels == max_label
+    
+def pca(xyz_hit):
+    '''
+    Performs a PCA fit to hit locations
+    Args:
+        xyz_hit - a Nx3 numpy array of hit locations
+    Returns:
+        2-``tuple`` of covariance matrix eigenvectors [3x3] and eigenvalues[3x1], sorted by decreasing eigenvalue
+    '''
+    cov = np.cov(xyz_hit, rowvar=False)
+    evals, evecs = np.linalg.eigh(cov)
+    return_order = np.argsort(evals)[::-1]
+#     print((evecs.T)[return_order], evals[return_order])
+    return (evecs.T)[return_order], evals[return_order]
+
+def compute_parity_flip(xyz_hit, vector, origin=(0,0,0)):
+    '''
+    Uses the dot product of the average xyz_hit vector and the specified vector to determine if vector in same, opposite, or parallel to most hits
+    Args:
+        xyz_hit - an Nx3 array of hit locations
+        vector - a length 3 comparison vector
+        origin - an optional offset to remove from each xyz_hit vector
+    Returns:
+        -1 if vector and avg xyz_hit vector are in opposite directions and +1 if in the same direction. 0 if they are perpendicular
+    '''
+    xyz_d = xyz_hit - origin
+    xyz_avg = np.mean(xyz_d, axis=0)
+    dot = np.dot(xyz_avg, vector)
+    if dot > 0:
+        return +1.
+    elif dot < 0:
+        return -1.
+    return 0.
+
+def do_calculation(data, vtx_data, radius=10., eps=2., min_samples=5, shower_label=2):
     '''
     Calculates the best fit direction of the gamma shower based on a PCA of the
     shower start
     Args:
-        data - a numpy array Nx5 containing hits associated with gamma shower
-        start - x,y,z of shower start (``tuple``)
-        length - the length of shower to include in the PCA (cm)
+        data - a numpy array Nx5 containing hits associated with gamma shower (x,y,z,batch,fivetypes label)
+        vtxs - a numpy array Mx5 containing shower vertexes (x,y,z,batch,label)
+        radius - hits within this radius are passed along to dbscan
+        eps - epsilon for dbscan
+        min_samples - min_samples for dbscan
+        shower_label - label number for filtering ``data`` by fivetypes label
     Returns:
-        a numpy array Nx7 containing [event, [x,y,z,batch,x_comp,y_comp,z_comp]]. XX_comp are the components of the unit vectors determined by the PCA
+        a numpy array Mx7 containing [event, [x,y,z,batch,x_comp,y_comp,z_comp]]. XX_comp are the components of the unit vectors determined by the PCA
     
     '''
-    # select hits within ``length`` of shower start
-    # perform PCA on those hits
-    pass
+    shower_hits_mask = data[:,-1] == shower_label
+    shower_hits = data[shower_hits_mask] # select shower-like hits
+    
+    return_data = np.empty((len(vtx_data),7))
+    for idx, vtx in enumerate(vtx_data):
+        filtered_hits_mask = norm(shower_hits[:,:3], vtx[:3]) < radius # select hits within a radius
+        filtered_hits = shower_hits[filtered_hits_mask]
+        associated_hits_mask = dbscan_find_primary(filtered_hits[:,:3], eps=eps, min_samples=min_samples) # associate hits with dbscan
+        associated_hits = filtered_hits[associated_hits_mask]
+        pca_vecs, pca_vals = pca(associated_hits[:,:3])
+        parity = compute_parity_flip(associated_hits[:,:3], pca_vecs[0], origin=vtx[:3]) # reverse vector in case it points in the antiparallel direction
+        return_data[idx] = np.array(list(vtx[:4]) + list(pca_vecs[0]*parity))
+    return return_data
---
+++ pi0/utils/pi0_pi_selection.py
@@ -0,0 +1,60 @@
+'''
+This module contains methods for making a pi+ track selection and (pi0 production vertex)
+
+'''
+import numpy as np
+
+from pi0.utils import gamma2_selection
+from pi0.utils import gamma_direction
+
+def calculate_vertex_loc(gamma0_vec, gamma1_vec):
+    '''
+    Use the backwards point of closest approach to determine a potential pi0 production vertex
+    Args:
+        gamma0_vec - length 6 vector of (x,y,z,dx,dy,dz)
+        gamma1_vec - length 6 vector of (x,y,z,dx,dy,dz)
+    Return:
+        length 4 vector of (x,y,z,l) of vertex, where l is the separation of the two gamma vectors at the vertex
+    
+    '''
+    print(gamma0_vec, gamma1_vec)
+    s0, s1, l = gamma2_selection.calculate_sep(gamma0_vec, gamma1_vec)
+    print('sep',s0,s1,l)
+    x0 = gamma0_vec[:3] + s0 * gamma0_vec[-3:]
+    x1 = gamma1_vec[:3] + s1 * gamma1_vec[-3:]
+    vtx = np.mean([x0, x1],axis=0)
+    return np.array([vtx[0], vtx[1], vtx[2], l])
+
+
+def do_selection(label_data, gamma0_data, gamma1_data, mip_label=1, tolerance=10.):
+    '''
+    Project 2 paired gammas back to their crossing point and return nearest (within tolerance) mip-track labeled hit as vertex candidate
+    Args:
+        label_data - Nx5 array with 5 being (x,y,z,batch,fivetypes label)
+        gamma0_data - 1x7 array with 7 being (x,y,z,batch,dx,dy,dz)
+        gamma1_data - 1x7 array with 7 being (x,y,z,batch,dx,dy,dz)
+        mip_label - fivetypes particle label for mip track to associate
+        tolerance - radius to search for a mip track label
+    Return:
+        length 5 array of (x,y,z,batch,l), l is the separation of the two gammas at the proposed vertex
+    '''
+    track_mask = label_data[:,-1] == mip_label
+    track_data = label_data[track_mask]
+    if not len(track_data):
+        return np.empty((0,5))
+    vtx_info = calculate_vertex_loc(gamma0_data, gamma1_data)
+    distances = gamma_direction.norm(track_data[:,:3], vtx_info[:3])
+    min_dist_idx = np.argmin(distances)
+    print('vtx',vtx_info)
+    print('closest',distances[min_dist_idx])
+    if distances[min_dist_idx] > tolerance:
+        return np.empty((0,5))
+    best_hit = track_data[min_dist_idx]
+    print('close hit',best_hit)
+    return_arr = best_hit
+    return_arr[4] = vtx_info[-1]
+    print('return data',return_arr)
+    return return_arr
+
+
+    
